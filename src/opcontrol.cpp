4e2fdf3d2c98a30cbb97f745147e5d846cb504f7
Akshay Kolwalkar <akshu-007@outlook.com>
2021-12-07 12:21:01 -0500

    Changed inputs for forklift and intake
    Decided to revert back to joystick for forklift and shoulder buttons for intake because forklift requires finer control.

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -32,12 +32,12 @@ void myOpControl() {
 
         driveTrain->arcade(joystickCubicDrive(forward), joystickCubicDrive(sideways), 0);
 
-        // Intake mapped to right joystick
-        int intakeSpeed = masterController.get_analog(ANALOG_RIGHT_Y);
+        // Intake mapped to right shoulder buttons
+        int intakeUp = masterController.get_digital(DIGITAL_R1);
+        int intakeDown = masterController.get_digital(DIGITAL_R2);
 
-        // Forklift mapped to left shoulder buttons
-        int forkliftUp = masterController.get_digital(DIGITAL_L1);
-        int forkliftDown = masterController.get_digital(DIGITAL_L2);
+        // Forklift mapped to right joystick
+        int forkliftSpeed = -(masterController.get_analog(ANALOG_RIGHT_Y));
 
         int intakeMacroCW = masterController.get_digital_new_press(DIGITAL_UP);
         int intakeMacroCCW = masterController.get_digital_new_press(DIGITAL_DOWN);
@@ -74,7 +74,18 @@ void myOpControl() {
                 // Operator control
                 intake.control();
 
-                intake.setPower(intakeSpeed);
+                //TODO: Update value 
+                int intakeSpeed = 100;
+
+                if (intakeUp) {
+                    intake.setPower(intakeSpeed);
+                }
+                else if (intakeDown) {
+                    intake.setPower(-(intakeSpeed));
+                }
+                else {
+                    intake.setPower(0);
+                }
             }
             default: {
                 break;
@@ -84,18 +95,9 @@ void myOpControl() {
 
         // Forklift control
         forklift.control();
-        
-        int forkliftSpeed = 127;
 
-        if (forkliftUp) {
-            forklift.setPower(forkliftSpeed);
-        }
-        else if (forkliftDown) {
-            forklift.setPower(-forkliftSpeed);
-        }
-        else {
-            forklift.setPower(0);
-        }
+        forklift.setPower(forkliftSpeed);
+
         // Run update funcs on sysmans
         forklift.update();
 
